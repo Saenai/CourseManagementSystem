83650eed975250dba37922ef6a021e5b8961c859
Saenai <22196758+Saenai@users.noreply.github.com>
2021-05-23 16:04:44 +0800

    add @vue/cli-plugin-vuex

--- vuecli/src/main.js
+++ vuecli/src/main.js
@@ -4,5 +4,6 @@ import './plugins/bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
