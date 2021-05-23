0e74a38ed2e05ddfe28ce410f30a39045025d1e8
Saenai <22196758+Saenai@users.noreply.github.com>
2021-05-23 16:39:58 +0800

    reinstall bootstrap-vue step 1

--- vuecli/src/main.js
+++ vuecli/src/main.js
@@ -1,9 +1,15 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import './plugins/bootstrap-vue'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import "./plugins/bootstrap-vue";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(BootstrapVue)
+  .mount("#app");
